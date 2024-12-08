09f44f28e64cf79ebbe8cf0e2d0cf5ea21a0d99b
zia99 <muhammad.zia@codistan.org>
2024-12-08 09:04:42 +0500

    Revert ""minor ui fixes in the trading-view and dashboard sidebar""
    This reverts commit 3d03b7384933aaf5f54d093bef6c6e3ac062b27f.

--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import Logo from "../../assets/icons/logoMain.svg";
 import { Button } from "@mui/material";
-import AccountShopIcon from "@components/assets/AccountShopIcon";
-import LogoutIcon from "@components/assets/LogoutIcon";
 
 const Sidebar = ({ menuItem }) => {
   const location = useLocation();
@@ -24,52 +22,42 @@ const Sidebar = ({ menuItem }) => {
           <img src={Logo} alt="CoinProp" className="w-[70px]" />
         </div>
       </NavLink>
-      <div className="hidden lg:block pb-4 pr-6 pt-[27px]">
+      <div
+        className="hidden lg:block pb-4 pr-6 pt-[27px]"
+        
+      >
         {menuItem?.map((item) => {
           const active = isNavLinkActive(item.path);
           const isFAQ = item.name === "Go to FAQ";
 
           return (
             <div key={item.id}>
-              <NavLink
-                to={item.path}
-                className={`font-medium text-base ${
-                  active
-                    ? "flex items-center rounded-lg  w-[99.5%] 5xl:w-[98%] text-custom-color border-l-8 border-l-[#01FF9D] border-y-2 border-r-2 border-r-[#4d4d4d] border-y-[#4d4d4d]"
-                    : "flex items-center hover:text-[#01FF9D]"
-                }
-          ${
-            item.name == "Affiliate Portal"
-              ? "border-b-2 border-b-custom-border"
-              : ""
+        <NavLink
+          to={item.path}
+          className={`font-medium text-base ${
+            active
+              ? "flex items-center rounded-lg  w-[99.5%] 5xl:w-[98%] text-custom-color border-l-8 border-l-[#01FF9D] border-y-2 border-r-2 border-r-[#4d4d4d] border-y-[#4d4d4d]"
+              : "flex items-center hover:text-[#01FF9D]"
           }`}
-              >
-                <div
-                  className={`flex gap-[10px] items-center py-3 ${
-                    active ? "pl-4" : "pl-8"
-                  }`}
-                >
-                  <div>
-                    {/* Pass the active prop to icons */}
-                    {React.isValidElement(item.icon)
-                      ? React.cloneElement(item.icon, { active })
-                      : item.icon}
-                  </div>
-                  <div>{item.name}</div>
-                </div>
-              </NavLink>
+        >
+          <div
+            className={`flex gap-[10px] items-center py-3 ${
+              active ? "pl-4" : "pl-8"
+            }`}
+          >
+            <div>
+              {/* Pass the active prop to icons */}
+              {React.isValidElement(item.icon)
+                ? React.cloneElement(item.icon, { active })
+                : item.icon}
             </div>
+            <div>{item.name}</div>
+          </div>
+        </NavLink>
+      </div>
+           
           );
         })}
-        <button class="mt-28 ms-5 bg-gradient-text text-black px-4 py-2 rounded-2xl flex justify-center items-center text-sm">
-         <AccountShopIcon />
-          Account Shop
-        </button>
-
-        <button class="ms-2 mt-2 flex text-sm text-white px-4 py-2 rounded-md">
-          <LogoutIcon />
-          Log Out
-        </button>
       </div>
     </>
   );
