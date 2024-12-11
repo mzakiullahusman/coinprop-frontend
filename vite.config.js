a24e9fcf55a606ed87cab42541dc5362d94e7a86
Muhammad Zakiullah Usman <musmanzaki@gmail.com>
2024-12-12 01:10:42 +0500

    build issues fixed

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,32 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@icons': path.resolve(__dirname, 'src/assets/icons'),
-      '@constants': path.resolve(__dirname, 'src/constants'),
-      '@context': path.resolve(__dirname, 'src/context'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@services': path.resolve(__dirname, 'src/services'),
-      '@theme': path.resolve(__dirname, 'src/theme'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
+      "@": path.resolve(__dirname, "src"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@icons": path.resolve(__dirname, "src/assets/icons"),
+      "@constants": path.resolve(__dirname, "src/constants"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@services": path.resolve(__dirname, "src/services"),
+      "@theme": path.resolve(__dirname, "src/theme"),
+      "@utils": path.resolve(__dirname, "src/utils"),
     },
   },
-  base: './',
-  optimizeDeps: {
-    exclude: ['chunk-AECWKNT7.js'], // Replace with the actual dependency causing the issue
-  },
+  base: "./",
   server: {
     watch: {
-      ignored: ['**/node_modules/**'], // Avoid unnecessary recompilation
+      ignored: ["**/node_modules/**"], // Avoid unnecessary recompilation
     },
   },
   build: {
-    outDir: 'dist',
-    sourcemap: true, // Enable source maps for better debugging
+    outDir: "dist",
+    sourcemap: false,
   },
 });
